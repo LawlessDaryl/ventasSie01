d97c62af6d18c62166198e2a4d4a4ddbdf0db10c
cleverbe <cleverbernal123@gmail.com>
2022-02-07 09:35:29 -0400

    asd

--- routes/web.php
+++ routes/web.php
@@ -51,9 +51,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('pos', PosController::class)->name('ventas');
 
     Route::group(['middleware' => ['role:ADMIN']], function () {
-        
-    });
-    Route::get('roles', RolesController::class)->name('roles');
+        Route::get('roles', RolesController::class)->name('roles');
         Route::get('permisos', PermisosController ::class)->name('permisos');
         Route::get('asignar', AsignarController::class)->name('asignar');
         Route::get('companies', CompaniesController::class)->name('empresa');
@@ -63,8 +61,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('cortecajas', CorteCajaController::class)->name('cortecaja');
         Route::get('plataformas', PlataformasController::class)->name('plataforma');
         Route::get('strproveedores', StrProveedorController::class)->name('proveedor');
-
-
+    });
 
     Route::get('users', UsersController::class)->name('usuarios');
     Route::get('cashout', CashoutController::class)->name('cashout');
