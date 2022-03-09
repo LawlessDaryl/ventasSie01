6e911f55ce1d49bf7ab9d29a05a61b9bc3e908d8
cleverbe <cleverbernal123@gmail.com>
2022-03-09 17:10:43 -0400

    archivos faltantes inventarios

--- app/Http/Livewire/ComprasController.php
+++ app/Http/Livewire/ComprasController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
 
 class ComprasController extends Component
 {
+    use WithPagination;
+    use WithFileUploads;
+    public  $nro_compra;
+
+    private $pagination = 5;
+    public function mount()
+    {
+        $this->nro_compra = 00200;
+     
+    }
     public function render()
     {
-        return view('livewire.compras.component');
+        return view('livewire.compras.component')
+        ->extends('layouts.theme.app')
+        ->section('content');
     }
 }
--- resources/views/livewire/compras/component.blade.php
+++ resources/views/livewire/compras/component.blade.php
@@ -3,29 +3,66 @@
         <div class="col-sm-12" >
             <div class="widget widget-chart-one">
                 <div class="widget-heading">
-                    <h2 class="card-title">
-                        <b>DETALLE COMPRA COD:{{$nro_compra}}</b>
-                    </h2>
-                    <div class="card component-card_1">
-                        <div class="card-body">
-                            <h5 class="card-title">Datos Proveedor</h5>
-                            <p class="card-text">
-                              <h6>Nombre:</h6>
-                              <h6>Codigo Proveedor:</h6>
-                              <h6>Fecha creacion:</h6>
-                            </p>
+                    <div class="col-9 col-xl-6 col-lg-12 mb-xl-5 mb-5 ">
+                        <div>
+                            <h5 class="mb-2 mt-2">Detalle Compra COD: {{$nro_compra}}</h5>
                         </div>
+                        <div class="d-flex b-skills">
+                            <div class="infobox border border-dark rounded pl-2 pr-5">
+                                <b class="info-text">Proveedor: </b> 
+                       
+                            NO DEFINIDO 
+                             <br/>
+                        
+                                <b class="info-text">Fecha: </b><br/>
+                              
+                                <b class="info-text">Registrado por: </b> <br/>
+                               
+                              </div>
+                        </div>
+
                     </div>
-                 
                     <ul class="tabs tab-pills">
-                        <a href="javascript:void(0)" class="btn btn-dark" data-toggle="modal"
+                        <a href="javascript:void(0)" class="btn btn-dark m-2" data-toggle="modal"
                             data-target="#theModal">Agregar Proveedor</a>
-                    </ul>
-                    <ul class="tabs tab-pills">
-                        <a href="javascript:void(0)" class="btn btn-dark" data-toggle="modal"
-                            data-target="#theModal">Generar Orden de Compra
+                
+                        <a href="javascript:void(0)" class="btn btn-dark m-2" data-toggle="modal"
+                            data-target="#theModal">Crear Producto
                         </a>
                     </ul>
+                </div>
+                <div class="widget-content">
+                    <div class="row">
+                        <div class="col-5">
+                                <div class="widget">
+                                    <div class="col-lg-12 col-12 col-md-4 col-sm-12">
+                                        <div class="input-group mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text input-gp">
+                                                    <i class="fas fa-search"></i>
+                                                </span>
+                                            </div>
+                                            <input type="text" wire:model="search" placeholder="Buscar" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 col-12 col-md-4 col-sm-12">
+                                        <h6 class="border border-success rounded">
+                                            <b>Elementos encontrados:</b> 0 Elementos encontrados
+                                        </h6>
+                                    </div>
+                                </div>
+                            
+                            
+                        </div>
+                   
+                        <div class="col-6">
+                            <div class="widget">
+                                <div class="col-lg-12 col-12 col-md-4 col-sm-12">
+
+                                </div>
+                            </div>
+                        </div>
+
                 </div>
 
             </div>
