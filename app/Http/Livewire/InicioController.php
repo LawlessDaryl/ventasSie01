0ff2fe6cadb67840e8a0990246d1ed921cf425cf
cleverbe <cleverbernal123@gmail.com>
2022-03-17 14:33:31 -0400

    m

--- app/Http/Livewire/InicioController.php
+++ app/Http/Livewire/InicioController.php
@@ -69,6 +69,22 @@ class InicioController extends Component
 
     public function render()
     {
+        $data = Caja::join('sucursals as s', 's.id', 'cajas.sucursal_id')
+            ->join('sucursal_users as su', 'su.sucursal_id', 's.id')
+            ->join('carteras as car', 'cajas.id', 'car.caja_id')
+            ->join('cartera_movs as cartmovs', 'car.id', 'cartmovs.cartera_id')
+            ->join('movimientos as mov', 'mov.id', 'cartmovs.movimiento_id')
+            ->where('mov.user_id', Auth()->user()->id)
+            ->where('mov.status', 'ACTIVO')
+            ->where('mov.type', 'APERTURA')
+            ->select('cajas.*', 's.name as sucursal')
+            ->get()->take(1);
+
+        if ($data->count() == 0) {
+            session(['sesionCaja' => null]);
+        } else {
+            session(['sesionCaja' => $data[0]->nombre]);
+        }
         $this->horaActual = date("d-m-y H:i:s ");
 
         /* dd($horaActual); */
