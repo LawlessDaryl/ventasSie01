ef45b90ac5f5a7de7dd0cf00ba279fa9b0b9e0d6
chio456789 <roscee777@gmail.com>
2022-03-21 22:10:05 -0400

    filtros

--- app/Http/Livewire/DestinoController.php
+++ app/Http/Livewire/DestinoController.php
@@ -4,28 +4,46 @@ namespace App\Http\Livewire;
 
 use App\Models\Location;
 use App\Models\ProductosDestino;
+use App\Models\Sucursal;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class DestinoController extends Component
 {
     use WithPagination;
+
+    public $selected_id,$selected_ubicacion;
     private $pagination = 10;
     public function paginationView()
     {
         return 'vendor.livewire.bootstrap';
     }
+
+
+  
+
     public function render()
     {
-        
-        $almacen= ProductosDestino::join('products as p','p.id','productos_destinos.product-id')
-                                    ->join('locations as loc','loc.id','productos_destinos.destino-id')
-                                    ->join('sucursals as suc','suc.id','loc.sucursal_id')
-                                    ->select('productos_destinos.*','p.nombre as name','loc.ubicacion','suc.id')
-                                    ->where('loc.ubicacion','ALMACEN')
-                                    ->orderBy('p.nombre','desc')
-                                    ->paginate($this->pagination);
+        if($this->selected_id !== null && $this->selected_ubicacion !== null )
 
+            $almacen= ProductosDestino::join('products as p','p.id','productos_destinos.product-id')
+                                        ->join('locations as loc','loc.id','productos_destinos.destino-id')
+                                        ->join('sucursals as suc','suc.id','loc.sucursal_id')
+                                        ->select('productos_destinos.*','loc.*','p.nombre as name','loc.ubicacion as ubi','suc.name as suc_id','loc.codigo as codigo')
+                                        ->where('loc.ubicacion',$this->selected_ubicacion)
+                                        ->where('suc.name',$this->selected_id)
+                                        ->orderBy('p.nombre','desc')
+                                        ->paginate($this->pagination);
+        else
+        $almacen= ProductosDestino::join('products as p','p.id','productos_destinos.product-id')
+        ->join('locations as loc','loc.id','productos_destinos.destino-id')
+        ->join('sucursals as suc','suc.id','loc.sucursal_id')
+        ->select('productos_destinos.*','loc.*','p.nombre as name','loc.ubicacion as ubi','suc.name as suc_id','loc.codigo as codigo')
+        ->where('loc.ubicacion','ALMACEN')
+        ->where('suc.id',1)
+        ->orderBy('p.nombre','desc')
+        ->paginate($this->pagination);
+      
         /*$products = Product::join('categories as c', 'c.id', 'products.category_id')
                 ->select('products.*', 'c.name as category')
                 ->where('products.name', 'like', '%' . $this->search . '%')
@@ -42,7 +60,11 @@ class DestinoController extends Component
                                     ->orderBy('p.nombre','desc')
                                     ->paginate($this->pagination);
 
-        return view('livewire.destino.destino-controller',['destinos_almacen'=>$almacen, 'destinos_tienda'=>$tienda])  
+                                    $suc_data=Sucursal::select('sucursals.*')
+                                    ->orderBy('sucursals.id','asc');
+                                    
+
+        return view('livewire.destino.destino-controller',['destinos_almacen'=>$almacen, 'destinos_tienda'=>$tienda, 'data_suc' => $suc_data->get()])  
         ->extends('layouts.theme.app')
         ->section('content');
     }
--- resources/views/livewire/destino/destino-controller.blade.php
+++ resources/views/livewire/destino/destino-controller.blade.php
@@ -8,40 +8,71 @@
                 <ul class="tabs tab-pills">
                     
                         <a href="javascript:void(0)" class="btn btn-dark" data-toggle="modal"
-                        data-target="#theModal">Mover Mercancia</a>
+                        data-target="#theModal">Transferir <br/>Productos</a>
                     
                 </ul>
             </div>
-            @include('common.searchbox')
+          
+
+            {{--SELECT DE LAS SUCURSALES--}}
+            <div class="row">
+                <div class="col-12 col-lg-4 col-md-6">
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text input-gp">
+                                        <i class="fas fa-search"></i>
+                                    </span>
+                                </div>
+                                <input type="text" wire:model="search" placeholder="Buscar" class="form-control">
+                            </div>
+                      
+                   
+                </div>
+                <div class="col-12 col-lg-3 col-md-3">
+
+                    <div class="form-group">
+                        <select wire:model='selected_id' class="form-control">
+                          <option value="null">Elegir Sucursal</option>
+                          @foreach ($data_suc as $data)
+                          <option value="{{ $data->name }}">{{ $data->name }}</option>
+                          @endforeach
+                        </select>
+                      </div>
+                </div>
+                <div class="col-12 col-lg-3 col-md-3">
+
+                    <div class="form-group">
+                        <select wire:model='selected_ubicacion' class="form-control">
+                          <option value="null">Elegir Ubicacion</option>
+                    
+                          <option value="TIENDA">TIENDA</option>
+                          <option value="ALMACEN">ALMACEN</option>
+                      
+                        </select>
+                      </div>
+                </div>
+
+            </div>
+          
 
             <div class="widget-content">
-                <div class="row">
+                <div class="d-flex row">
 
-                    <div class="col-12 col-lg-6 col-md-4 border rounded">
-                      <h3>Almacen</h3>
+                    <div class="col-12 col-lg-12 col-md-4">
+                    
                       @foreach($destinos_almacen as $destino)
                       
-                        <div>
-                           <h3> {{$destino->name}}</h3>
-                           <h5>{{$destino->ubicacion}}</h5>
-                           <h5>{{$destino->stock}}</h5>
-                           <h5>{{$destino->nombre}}</h5>
-                        </div>
+                        <div class="card border-success m-2" style="max-width: 18rem;">
+                            <div class="card-header"><h3> {{$destino->suc_id}}</h3></div>
+                            <div class="card-body text-success">
+                              <h5 class="card-title">{{$destino->tipo}}-{{$destino->codigo}}</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
                       @endforeach
 
                     </div>
-                    <div class="col-12 col-lg-6 col-md-4 border rounded">
-                        <h3>Tienda</h3>
-                        @foreach($destinos_tienda as $destino)
-                      
-                        <div>
-                           <h3> {{$destino->name}}</h3>
-                           <h5>{{$destino->ubicacion}}</h5>
-                           <h5>{{$destino->stock}}</h5>
-                        </div>
-                      @endforeach
-
-                  </div>
+                  
                 </div>
             </div>
         </div>
