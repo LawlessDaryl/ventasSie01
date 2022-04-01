3858c0d7f08211d20254b1c442192519f63c83c6
Fenris <h666heavymetal@gmail.com>
2022-04-01 13:01:24 -0400

    asd

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -190,7 +190,8 @@ class PosController extends Component
     ];
     public function ScanCode($barcode, $cant = 1)
     {
-        //Escanea el Código
+        //ssfd
+        //Escanea el Códigodfsdfsdfsdf
         $product = Product::join("productos_destinos as pd", "pd.product_id", "products.id")
         ->join('locations as d', 'd.id', 'pd.location_id')
         ->join('destinos as des', 'des.id', 'd.destino_id')
